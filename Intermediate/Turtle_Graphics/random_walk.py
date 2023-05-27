1324f5ee088fcfc6abd514693369700f1004f3cc
sevkw <kwang0422@gmail.com>
2023-05-26 22:36:40 -0400

    add turtle graphics project

---
+++ Intermediate/Turtle_Graphics/random_walk.py
@@ -0,0 +1,22 @@
+import turtle as t
+from random import randint
+from random import choice
+def random_direction(turtle):
+    """A function to randomnize the heading direction."""
+    direction_angle = [0, 90, 180, 270]
+    to_angle = choice(direction_angle)
+    turtle.setheading(to_angle)
+
+
+rt = t.Turtle()
+rt.pensize(5)
+rt.speed("fast")
+screen = t.Screen()
+screen.colormode(255)
+
+while True:
+    random_direction(rt)
+    rt.pencolor((randint(0, 255), randint(0, 255), randint(0, 255)))
+    rt.forward(20)
+
+screen.exitonclick()
