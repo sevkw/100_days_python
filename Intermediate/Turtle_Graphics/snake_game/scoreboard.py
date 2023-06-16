88f3c46f1dfc23f9f91942b2cbdd67f863191131
sevkw <kwang0422@gmail.com>
2023-06-15 20:48:53 -0400

    improve snake game

--- Intermediate/Turtle_Graphics/snake_game/scoreboard.py
+++ Intermediate/Turtle_Graphics/snake_game/scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.hideturtle()
         self.color('white')
         self.penup()
@@ -15,13 +16,21 @@ class Scoreboard(Turtle):
         self.refresh_scoreboard()
     
     def refresh_scoreboard(self):
-        self.write(f"Score: {self.score}", move=False, align=ALIGN, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGN, font=FONT)
 
     def update_score(self):
         self.clear()
         self.score += 1
         self.refresh_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER!", move=False, align=ALIGN, font=FONT)
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER!", move=False, align=ALIGN, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        ## to reset the score for the game instance to 0
+        self.score = 0
+        self.refresh_scoreboard()
