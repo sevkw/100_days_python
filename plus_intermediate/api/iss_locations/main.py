809b0f29cd1dc047da964353a26c31b2e5754c0e
sevkw <kwang0422@gmail.com>
2023-06-29 21:41:16 -0400

    add various api projects

---
+++ plus_intermediate/api/iss_locations/main.py
@@ -0,0 +1,14 @@
+# ------------------- Basic Requests --------------------- #
+# import requests
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+# # print(response)
+
+# data = response.json()
+
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+# iss_position = (longitude, latitude)
+# print(iss_position)
+
