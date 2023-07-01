1407c6d5ca82dfa2a0bc888f9f09b1f14e5c5c47
sevkw <kwang0422@gmail.com>
2023-07-01 19:30:53 -0400

    update ui.py

--- plus_intermediate/tkinter_projects/trivia_quiz_app/ui.py
+++ plus_intermediate/tkinter_projects/trivia_quiz_app/ui.py
@@ -12,17 +12,17 @@ class QuizInterface:
         self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
 
         self.quiz_text = self.canvas.create_text(150, 125, text="Quiz Text", font=(FONT, 16, "italic"), fill="black")
-        self.canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
+        self.canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=50)
 
-        self.label = Label(text="Score: ", font=(FONT,10, "bold"), bg=THEME_COLOR, fg="white")
-        self.label.grid(row=0, column=1)
+        self.score_label = Label(text="Score: ", font=(FONT,10, "bold"), bg=THEME_COLOR, fg="white")
+        self.score_label.grid(row=0, column=1)
 
-        self.check_img = PhotoImage(file=r".\images\true.png")
-        self.cross_img = PhotoImage(file=r".\images\false.png")
-        self.tick_button = Button(image=self.check_img, highlightthickness=0)
-        self.tick_button.grid(row=2, column=0, padx=20, pady=20)
-        self.cross_button = Button(image=self.cross_img, highlightthickness=0)
-        self.cross_button.grid(row=2, column=1, padx=20, pady=20)
+        check_img = PhotoImage(file=r".\images\true.png")
+        cross_img = PhotoImage(file=r".\images\false.png")
+        self.true_button = Button(image=check_img, highlightthickness=0)
+        self.true_button.grid(row=2, column=0, padx=20, pady=20)
+        self.false_button = Button(image=cross_img, highlightthickness=0)
+        self.false_button.grid(row=2, column=1, padx=20, pady=20)
 
 
 
