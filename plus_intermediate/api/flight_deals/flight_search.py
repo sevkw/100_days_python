6fd16d8a840a1b4a5e413aa7c6734c949476168e
sevkw <kwang0422@gmail.com>
2023-07-08 12:27:09 -0400

    update flight_search project

--- plus_intermediate/api/flight_deals/flight_search.py
+++ plus_intermediate/api/flight_deals/flight_search.py
@@ -1,3 +1,29 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+
+load_dotenv(".env.flights")
+
+FLIGHT_SEARCH_APIKEY = os.getenv("FLIGHT_SEARCH_APIKEY")
+FLIGHT_SEARCH_ENDPOINT = os.getenv("FLIGHT_SEARCH_ENDPOINT")
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    """This class is responsible for talking to the Flight Search API.
+    API reference: https://tequila.kiwi.com/portal/docs/tequila_api
+    """
+    def __init__(self):
+        self.endpoint = FLIGHT_SEARCH_ENDPOINT
+        self.header = { "apikey": FLIGHT_SEARCH_APIKEY}
+    
+    def get_iatacode(self, city_name:str) -> str:
+        """Get and return the IATA CODE from City Name"""
+        location_endpoint = f"{self.endpoint}/locations/query"
+        param = {
+            "term": city_name,
+            "location_types": "city"
+        }
+        response = requests.get(url=location_endpoint, headers=self.header, params=param).json()
+        iata_code = response["locations"][0]["code"]
+        return iata_code
--- plus_intermediate/api/flight_deals/main.py
+++ plus_intermediate/api/flight_deals/main.py
@@ -1,16 +1,20 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from data_manager import DataManager
+from flight_search import FlightSearch
 
 from pprint import pprint
 
 data_manager = DataManager()
 sheet_data = data_manager.get_data()
+flight_search = FlightSearch()
 # pprint(sheet_data)
 # pprint(sheet_data[0]['iataCode'])
 
 for data in sheet_data:
     if data["iataCode"] == "":
-        data['iataCode'] = "TESTING"
+        city_name = data["city"]
+        city_code = flight_search.get_iatacode(city_name)
+        data['iataCode'] = city_code
         update_code = {'iataCode' : data['iataCode']}
         row_id = data['id']
         data_manager.update_data(row_id, update_code)
