439b1e23105da84726e9d6882d3ab26ce7d7a261
sevkw <kwang0422@gmail.com>
2023-07-15 12:29:10 -0400

    add exercises of beautiful soup

---
+++ plus_intermediate/web_scraping/beautiful_soup/exercises_1/main.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+
+with open("website.html", encoding="utf8") as fp:
+    contents = fp.read()
+
+soup = BeautifulSoup(contents, "html.parser")
+
+# print(soup.title.string)
+
+all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+
+# for tag in all_anchor_tags:
+    # print(tag.getText())
+    # print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+# print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading)
+# print(section_heading.get("class"))
+
+## select a specific element
+## think of the selector as the selector in CSS
+company_url = soup.select_one(selector="p a")
+# print(company_url)
+
+name = soup.select_one(selector="#name")
+# print(name)
+
+## select all elements that are class: heading
+headings = soup.select(".heading")
+print(headings)
+
