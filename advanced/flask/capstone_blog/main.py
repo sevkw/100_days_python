178112177a7161667521789384af27a122b07894
sevkw <kwang0422@gmail.com>
2023-08-15 21:53:28 -0400

    update jinja for loop in index.html

--- advanced/flask/capstone_blog/main.py
+++ advanced/flask/capstone_blog/main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 import requests
 
+blog_url = "https://api.npoint.io/9f0ec336aa9464fe79cb"
+all_posts = requests.get(blog_url).json()
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=all_posts)
 
 @app.route('/about')
 def get_about():
