23d3a3a3845ea952bbd2b5a1100ffcac4132873f
sevkw <kwang0422@gmail.com>
2023-08-20 14:37:34 -0400

    update flask_wtforms exercise code

--- advanced/flask/flask_wtforms/main.py
+++ advanced/flask/flask_wtforms/main.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
 from flask_wtf.csrf import CSRFProtect
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(max=30)])
-    password = StringField('Password', validators=[DataRequired(), Length(max=30)])
+    email = StringField(label='Email', validators=[DataRequired(), Email(message="Please ensure Email Address entered is valid!", check_deliverability=True)])
+    # password = StringField('Password', validators=[DataRequired(), Length(max=30)])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8, message="Field must be at least 8 characters long.")])
+    submit = SubmitField(label = 'Log In')
 
 
 app = Flask(__name__)
@@ -17,7 +19,10 @@ app.secret_key = "123456"
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return "<h1>Login Successful!</h1>"
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return "<h1>Login Successful!</h1>"
+        else:
+            return "<h1>Access Denied</h1>"
     return render_template('login.html', form=form)
 
 
