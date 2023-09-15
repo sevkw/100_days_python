877ebd5587bd7d215eff1721e8b94fca32d18cf5
sevkw <kwang0422@gmail.com>
2023-09-14 21:43:05 -0400

    add ability to delete existing post

--- advanced/flask/upgraded_blog/main.py
+++ advanced/flask/upgraded_blog/main.py
@@ -72,7 +72,6 @@ def add_new_post():
         return redirect(url_for('get_all_posts'))
     return render_template("make-post.html", form=form)
 
-# TODO: edit_post() to change an existing blog post
 @app.route('/edit-post/<int:post_id>', methods=["GET", "POST"])
 def edit_post(post_id):
     post = db.get_or_404(BlogPost, post_id)
@@ -90,11 +89,19 @@ def edit_post(post_id):
         post.author = edit_form.author.data
         post.body = edit_form.body.data
         db.session.commit()
+        
         return redirect(url_for('show_post', post_id = post.id))
+    
     return render_template("make-post.html", form=edit_form, is_edit=True)
-# TODO: delete_post() to remove a blog post from the database
 
-# Below is the code from previous lessons. No changes needed.
+@app.route("/delete/<int:post_id>")
+def delete_post(post_id):
+    post = db.get_or_404(BlogPost, post_id)
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect(url_for('get_all_posts'))
+
 @app.route("/about")
 def about():
     return render_template("about.html")
