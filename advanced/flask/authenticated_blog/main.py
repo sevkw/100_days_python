a5b09b25d0a38b320369d3b7867d1a8ab3a808a5
sevkw <kwang0422@gmail.com>
2023-09-23 16:10:46 -0400

    add relationship to db

--- advanced/flask/authenticated_blog/main.py
+++ advanced/flask/authenticated_blog/main.py
@@ -35,6 +35,10 @@ db.init_app(app)
 class BlogPost(db.Model):
     __tablename__ = "blog_posts"
     id = db.Column(db.Integer, primary_key=True)
+    
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    author = relationship("User", back_populates="posts")
+    
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
@@ -51,6 +55,7 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(250), nullable=False)
     name = db.Column(db.String(250), nullable=False)
 
+    posts = relationship("BlogPost", back_populates="author")
 
 with app.app_context():
     db.create_all()
